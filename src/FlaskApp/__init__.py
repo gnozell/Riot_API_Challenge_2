c3640409ea7a620210299a1a1f5a4db6a5ee8a70
griffinplex <griffin.nozell@plexxi.com>
2015-08-17 11:19:50 -0400

    FlaskApp
    relocated static to under flask app

--- src/FlaskApp/__init__.py
+++ src/FlaskApp/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask, send_from_directory, render_template
 import os
 
-app = Flask(__name__, static_folder='../../rel', static_url_path='')
+app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,7 +14,8 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    #return render_template('500.html'), 500
+    return "lol you 500"
 
 if __name__ == "__main__":
     app.run()
