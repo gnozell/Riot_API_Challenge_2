18ada4c8a2c0d1b1af9ad3231c0b6b50207a0ce5
griffinplex <griffin.nozell@plexxi.com>
2015-08-17 12:12:30 -0400

    __init__ pages
    set up the flask app to work with static folder and to serve up my
    webpages.

--- src/FlaskApp/__init__.py
+++ src/FlaskApp/__init__.py
@@ -1,21 +1,40 @@
 from flask import Flask, send_from_directory, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static/', static_url_path='')
 
 @app.route("/")
+@app.route("/index.html")
 def homepage():
     return render_template('index.html')
 
+@app.route("/about.html")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/black_market.html")
+def black_market_page():
+    return render_template('black_market.html')
+
+@app.route("/brawlers.html")
+def brawlers_page():
+    return render_template('brawlers.html')
+
+@app.route("/champion.html")
+def champion_page():
+    return render_template('champion.html')
+
+@app.route("/champion_info.html")
+def champion_info_page():
+    return render_template('champion_info.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
-	#return render_template('404.html'), 404
-	return "lol you 404"
+	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    #return render_template('500.html'), 500
-    return "lol you 500"
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.run()
